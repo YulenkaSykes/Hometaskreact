52c6a98f06399cdf6b48fd3d2c081e6d16e8ddf6
YulenkaSykes <72409591+YulenkaSykes@users.noreply.github.com>
2021-10-05 16:00:47 +0300

    fix

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,19 +1,18 @@
-import {useState} from 'react';
-import React from 'react'
-
+import { useState } from "react";
+import React from "react";
 
 export default function Todo(props) {
-    const {todo, n, deleteTodo} = props;
-    const [done, setDone] = useState(false)
+  const { todo, n, deleteTodo } = props;
+  const [done, setDone] = useState(false);
 
-    return (
-        <div className={`todos ${done &&"done"}`}>
-            <div className="list">
-            <input type="checkbox" onChange={()=> setDone(!done)} />
-            <span>{n}.</span>
-            <span>{todo}</span>
-            </div>
-            <button onClick={()=> deleteTodo(todo)}>Delete</button>
-        </div>
-    )
+  return (
+    <div className={`todos ${done && "done"}`}>
+      <div className="list">
+        <input type="checkbox" onChange={() => setDone(!done)} />
+        <span>{n}.</span>
+        <span>{todo}</span>
+      </div>
+      <button onClick={() => deleteTodo(todo)}>Delete</button>
+    </div>
+  );
 }
