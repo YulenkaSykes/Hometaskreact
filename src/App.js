1ae0b86a68a2b4395ee2df592e493194cd81498f
YulenkaSykes <72409591+YulenkaSykes@users.noreply.github.com>
2021-11-03 17:29:14 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,49 +1,54 @@
-import { useEffect, useState } from 'react';
-import Todos from "./Components/Todos"
-import './App.css';
-
+import { useEffect, useState } from "react";
+import Todos from "./Components/Todos";
+import "./App.css";
 
 function Todo() {
   const [todos, setTodos] = useState([]);
-  const [value, setValue] = useState("")
-  const [search, setSearch] = useState("")
-  
+  const [value, setValue] = useState("");
+  const [search, setSearch] = useState("");
 
-  const handleChange = (e)=> {
-    setValue(e.target.value)
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
 
-  const handleClick = ()=> {
-    setTodos([...todos,value])
-    setValue("")
-  }
+  const handleClick = () => {
+    setTodos([...todos, value]);
+    setValue("");
+  };
 
-  const deleteTodo = (text)=> {
-    setTodos(todos.filter((e)=> e !== text))
-  }
+  const deleteTodo = (text) => {
+    setTodos(todos.filter((e) => e !== text));
+  };
 
   useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/todos")
-        .then((response) => response.json())
-        .then((data) => {
-          setTodos([...todos, ...data.map((item) => item.title)]);
-        });
-  },[]);
-
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos([...todos, ...data.map((item) => item.title)]);
+      });
+  }, []);
 
   return (
     <div className="todo">
       <h1>TodoShka</h1>
-     <div>
-       <input type="text" onChange={handleChange} value={value}/>
-       <button onClick={handleClick}>Add todo</button>
-     </div>
-     <h2>Search everything in the world</h2>
-     <input type="text" onChange={(e)=> {
-       setSearch(e.target.value)}} />
-     {search.length > 0 ?
-     todos.filter((e)=> e.includes(search)).map((e,i)=> <Todos n={i} todo={e} deleteTodo={deleteTodo}/>) :
-     todos.length > 0 && todos.map((e,i)=> <Todos n={i} todo={e} deleteTodo={deleteTodo}/>)}
+      <div>
+        <h1 className="create">Create todo</h1>
+        <input type="text" onChange={handleChange} value={value} />
+        <button onClick={handleClick}>Add todo</button>
+      </div>
+      <h2>Search everything in the world</h2>
+      <input
+        type="text"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      {search.length > 0
+        ? todos
+            .filter((e) => e.includes(search))
+            .map((e, i) => <Todos n={i} todo={e} deleteTodo={deleteTodo} />)
+        : todos.length > 0 &&
+          todos.map((e, i) => <Todos n={i} todo={e} deleteTodo={deleteTodo} />)}
     </div>
   );
 }
